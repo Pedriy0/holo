941663db5c23516993ee046e5945f956721ce98f
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-28 14:02:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
 var treasureCollection = 0;
 var cashG,diamondsG,jwelleryG,swordGroup;
 
-//Game States
+//Game States (estados del juego)
 var PLAY=1;
 var END=0;
 var gameState=1;
@@ -16,7 +16,7 @@ function preload(){
   jwelleryImg = loadImage("jwell.png");
   swordImg = loadImage("sword.png");
   gover = loadImage("gameOver.png");
-  //Uncomment correct line of code to load the image named gameOver.png
+  //Descomentar la línea de código correcta para cargar la omagen llamada gameOver.png
   
   //gover=gameOver.png;
   
@@ -28,14 +28,14 @@ function preload(){
 function setup(){
   
   createCanvas(400,600);
-// Moving background
+// Mover el fondo
   
 path=createSprite(200,200);
 path.addImage(pathImg);
 path.velocityY = 4;
 
 
-//creating boy running
+//crear sprite de niño corriendo
 boy = createSprite(70,580,20,20);
 boy.addAnimation("SahilRunning",boyImg);
 boy.scale=0.08;
@@ -57,7 +57,7 @@ function draw() {
   edges= createEdgeSprites();
   boy.collide(edges);
   
-  //code to reset the background
+  //código para reiniciar el fondo
   if(path.y > 400 ){
     path.y = height/2;
   }
@@ -93,7 +93,7 @@ function draw() {
         diamondsG.setVelocityYEach(0);
         jwelleryG.setVelocityYEach(0);
         swordGroup.setVelocityYEach(0);
-        //Uncomment correct section of code to display game over 
+        //Descomentar la sección de código correcta para mostrar game over 
 
         //  over = createSprite(880,9980,20,20);
         //  over.addAnimation("gameover",gover);
@@ -113,7 +113,7 @@ function draw() {
   drawSprites();
   textSize(20);
   fill(255);
-  text("Treasure: "+ treasureCollection,10,30);
+  text("Tesoro: "+ treasureCollection,10,30);
   }
 
 }
